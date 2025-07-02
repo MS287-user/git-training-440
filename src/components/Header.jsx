0e5b2bf056a2d508cb2ff38bb92749fb6e57bf4f
unknown <ms3071687@gmail.com>
2025-07-02 12:18:22 +0500

    Tailwind integration and Remixicon integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,24 @@ import React from "react";
 const Header = () => {
   return (
     <>
-        Header
+        <div className="p-4 flex justify-between items-center bg-blue-900 text-white">
+          <div id="nav-logo">
+            <i className="ri-behance-line text-2xl"></i>
+          </div>
+
+          <div id="nav-menu">
+            <ul className="md:flex hidden gap-5">
+              <li>Home</li>
+              <li>About</li>
+              <li>Services</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+
+          <div id="nav-toggle" className="md:invisible visible">
+            <i className="ri-menu-2-line text-2xl text-white"></i>
+          </div>
+        </div>
     </>
   )
 };
